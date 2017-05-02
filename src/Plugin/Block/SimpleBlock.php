658bf25a21e97708913efea343979d9e312a363b
Jose Jiménez <jose.jimenez@wunderkraut.com>
2017-05-02 23:12:24 +0200

    Add a simple block

---
+++ src/Plugin/Block/SimpleBlock.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\cache_dev\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'SimpleBlock' block.
+ *
+ * @Block(
+ *  id = "simple_block",
+ *  admin_label = @Translation("Simple block"),
+ * )
+ */
+class SimpleBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['simple_block']['#markup'] = 'Implement My new block.';
+
+    return $build;
+  }
+
+}
